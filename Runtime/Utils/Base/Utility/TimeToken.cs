6f07fdef599bea9666b9f8fc0ccce6e8e1e54e71
Mikhail Korchun <korchoon@gmail.com>
2019-07-29 12:03:57 +0300

    notice

--- Runtime/Utils/Base/Utility/TimeToken.cs
+++ Runtime/Utils/Base/Utility/TimeToken.cs
@@ -1,4 +1,10 @@
-﻿namespace Lib.Timers
+﻿// ----------------------------------------------------------------------------
+// The MIT License
+// Async Reactors framework https://github.com/korchoon/async-reactors
+// Copyright (c) 2017-2019 Mikhail Korchun <korchoon@gmail.com>
+// ----------------------------------------------------------------------------
+
+namespace Lib.Timers
 {
     public struct TimeToken
     {
